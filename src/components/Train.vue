70aac08a33326996c41e4d3a743fbe77987c47c0
JarryShu <2623574740@qq.com>
2020-12-28 14:06:40 +0800

    v-model实验

---
+++ src/components/Train.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Train">
+    输入<input type="text" v-model="msg" />
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Train',
+  props: {
+    
+  },data(){
+    return{
+      msg:''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
