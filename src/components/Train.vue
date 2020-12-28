13d7db318e3dd5ca497278ff38f8a252b1a124db
JarryShu <2623574740@qq.com>
2020-12-28 17:32:36 +0800

    训练了v-model

--- src/components/Train.vue
+++ src/components/Train.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="Train">
-    输入<input type="text" v-model="msg" />
-    {{msg}}
+    v-model输入1<input type="text" v-model="str_1" />&nbsp;&nbsp;&nbsp;
+    v-model输入2<input type="text" v-model="str_1" />&nbsp;&nbsp;&nbsp;显示{{str_1}}
+    <br/>
+    v-model.trim&nbsp;<input type="text" v-model.trim="str_2">&nbsp;&nbsp;&nbsp;长度：{{str_2.length}}
+    <br/>
+    v-model.number:<input type="text" v-model.number="num_1">&nbsp;&nbsp;&nbsp;转换：{{num_1}}
+    <br/>
+    v-model.lazy:<input type="text" v-model.lazy="num_2">&nbsp;&nbsp;&nbsp;转换：{{num_2}}
   </div>
 </template>
 
@@ -12,8 +18,12 @@ export default {
     
   },data(){
     return{
-      msg:''
+      str_1:'',
+      str_2:'',
+      num_1:0,
+      num_2:0
     }
+  },methods:{
   }
 }
 </script>
